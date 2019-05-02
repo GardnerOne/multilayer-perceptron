5208bb48e21e34c70e705bb3b8e17a83bdbaee93
Jamie Daniel Meredith Gardner <jgardner05@qub.ac.uk>
2019-05-02 14:16:51 +0100

    Update to ES6

--- matrix.js
+++ matrix.js
@@ -1,43 +1,42 @@
-function Matrix(rows, cols) {
-  this.rows = rows;
-  this.cols = cols;
-  this.matrix = [];
+class Matrix {
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
 
-  for (var i = 0; i < this.rows; i++) {
-    this.matrix[i] = [];
-    for (var j = 0; j < this.cols; j++) {
-      this.matrix[i][j] = 0;
-    }
-  }
-}
-
-Matrix.prototype.randomize = function () {
-  for (var i = 0; i < this.rows; i++) {
-    for (var j = 0; j < this.cols; j++) {
-      this.matrix[i][j] = Math.floor(Math.random() * 10);
-    }
-  }
-}
-
-Matrix.prototype.add = function (n) {
-  for (var i = 0; i < this.rows; i++) {
-    for (var j = 0; j < this.cols; j++) {
-      if (n instanceof Matrix) {
-        this.matrix[i][j] += n.matrix[i][j];
-      } else {
-        this.matrix[i][j] += n;
+    this.matrix = [];
+    for (var i = 0; i < this.rows; i++) {
+      this.matrix[i] = [];
+      for (var j = 0; j < this.cols; j++) {
+        this.matrix[i][j] = 0;
       }
     }
   }
-}
-
-Matrix.prototype.multiply = function (n) {
-  for (var i = 0; i < this.rows; i++) {
-    for (var j = 0; j < this.cols; j++) {
-      if (n instanceof Matrix) {
-        this.matrix[i][j] *= n.matrix[i][j];
-      } else {
-        this.matrix[i][j] *= n;
+  randomize() {
+    for (var i = 0; i < this.rows; i++) {
+      for (var j = 0; j < this.cols; j++) {
+        this.matrix[i][j] = Math.floor(Math.random() * 10);
+      }
+    }
+  }
+  add(n) {
+    for (var i = 0; i < this.rows; i++) {
+      for (var j = 0; j < this.cols; j++) {
+        if (n instanceof Matrix) {
+          this.matrix[i][j] += n.matrix[i][j];
+        } else {
+          this.matrix[i][j] += n;
+        }
+      }
+    }
+  }
+  multiply(n) {
+    for (var i = 0; i < this.rows; i++) {
+      for (var j = 0; j < this.cols; j++) {
+        if (n instanceof Matrix) {
+          this.matrix[i][j] *= n.matrix[i][j];
+        } else {
+          this.matrix[i][j] *= n;
+        }
       }
     }
   }
