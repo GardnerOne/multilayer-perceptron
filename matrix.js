c9d0e364e2a79e3ae0af19f3afbaeb2a42d4cb0d
Jamie Daniel Meredith Gardner <jgardner05@qub.ac.uk>
2019-05-06 15:06:40 +0100

    Refactor matrix class

--- matrix.js
+++ matrix.js
@@ -1,30 +1,33 @@
 class Matrix {
+
   constructor(rows, cols) {
     this.rows = rows;
     this.cols = cols;
+    this.data = [];
 
-    this.matrix = [];
     for (let i = 0; i < this.rows; i++) {
-      this.matrix[i] = [];
+      this.data[i] = [];
       for (let j = 0; j < this.cols; j++) {
-        this.matrix[i][j] = 0;
+        this.data[i][j] = 0;
       }
     }
   }
+
   randomize() {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
-        this.matrix[i][j] = Math.floor(Math.random() * 10);
+        this.data[i][j] = Math.floor(Math.random() * 10);
       }
     }
   }
+
   add(n) {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
         if (n instanceof Matrix) {
-          this.matrix[i][j] += n.matrix[i][j];
+          this.data[i][j] += n.data[i][j];
         } else {
-          this.matrix[i][j] += n;
+          this.data[i][j] += n;
         }
       }
     }
@@ -34,40 +37,53 @@ class Matrix {
     let result = new Matrix(this.cols, this.rows);
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
-        result.matrix[j][i] = this.matrix[i][j];
+        result.data[j][i] = this.data[i][j];
+      }
+    }
+    this = result;
+  }
+
+  multiply(n) {
+    // Scalar product
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.data[i][j] *= n;
+      }
+    }
+  }
+
+  print() {
+    console.table(this.data);
+  }
+
+  static multiply(m, n) {
+    if (m.cols !== n.rows) {
+      console.log("Columns of A must match rows of B");
+      return undefined;
+    }
+
+    let result = new Matrix(m.rows, n.cols);
+    for (let i = 0; i < result.rows; i++) {
+      for (let j = 0; j < result.cols; j++) {
+        let sum = 0;
+        for (let k = 0; k < m.cols; k++) { // or b.rows
+          // Dot product
+          sum += m.data[i][k] * n.data[k][j];
+        }
+        result.data[i][j] = sum;
       }
     }
     return result;
   }
 
-  multiply(n) {
-    if (n instanceof Matrix) {
-      if (this.cols !== n.rows) {
-        console.log("Columns of A must match rows of B");
-        return undefined;
-      }
 
-      let a = this;
-      let b = n;
-      let result = new Matrix(a.rows, b.cols);
-      for (let i = 0; i < result.rows; i++) {
-        for (let j = 0; j < result.cols; j++) {
-          let sum = 0;
-          for (let k = 0; k < a.cols; k++) { // or b.rows
-            // Dot product
-            sum += a.matrix[i][k] * b.matrix[k][j];
-          }
-          result.matrix[i][j] = sum;
-        }
-      }
-      return result;
-    } else {
-      // Scalar product
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] *= n;
-        }
+  static transpose(m) {
+    let result = new Matrix(m.cols, m.rows);
+    for (let i = 0; i < m.rows; i++) {
+      for (let j = 0; j < m.cols; j++) {
+        result.data[j][i] = m.data[i][j];
       }
     }
+    return result;
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
 	let a = new Matrix(2, 3);
 	a.randomize();
-	console.table(a.matrix);
-	let b = a.transpose();
-	console.table(b.matrix);
+	console.table(a.data);
+	let b = Matrix.transpose(a);
+	console.table(b.data);
+
+	let c = Matrix.multiply(a, b);
+	c.print();
 }
