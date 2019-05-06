8432bf394acf5f482033fbd5ec6cd184bcfb972a
Jamie Daniel Meredith Gardner <jgardner05@qub.ac.uk>
2019-05-06 14:44:46 +0100

    Add matrix transpose method

--- nn.js
+++ nn.js
@@ -4,4 +4,9 @@ class NeuralNetwork {
     this.hidden_nodes = hidden_nodes;
     this.output_nodes = output_nodes;
   }
+
+  FeedForward(input) {
+    // Matrix math
+    return guess;
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,7 @@
 function setup() {
 	let a = new Matrix(2, 3);
-	let b = new Matrix(3, 2);
-
 	a.randomize();
-	b.randomize();
-
 	console.table(a.matrix);
+	let b = a.transpose();
 	console.table(b.matrix);
-
-	let c = a.multiply(b);
-	console.table(c.matrix);
 }
